2c296c2b22e0a7d0a17e62d63132bb9f8a7c2818
CalinaBorzan <161467317+CalinaBorzan@users.noreply.github.com>
2024-05-05 21:11:36 +0300

    Add files via upload

---
+++ xsim.dir/calc_behav/obj/xsim_1.c
@@ -0,0 +1,137 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_256(char*, char *);
+extern void execute_257(char*, char *);
+extern void execute_250(char*, char *);
+extern void execute_45(char*, char *);
+extern void execute_47(char*, char *);
+extern void execute_81(char*, char *);
+extern void execute_82(char*, char *);
+extern void execute_83(char*, char *);
+extern void execute_84(char*, char *);
+extern void execute_85(char*, char *);
+extern void execute_86(char*, char *);
+extern void execute_87(char*, char *);
+extern void execute_88(char*, char *);
+extern void execute_215(char*, char *);
+extern void execute_218(char*, char *);
+extern void execute_219(char*, char *);
+extern void execute_220(char*, char *);
+extern void execute_221(char*, char *);
+extern void execute_222(char*, char *);
+extern void execute_223(char*, char *);
+extern void execute_224(char*, char *);
+extern void execute_225(char*, char *);
+extern void execute_226(char*, char *);
+extern void execute_227(char*, char *);
+extern void execute_228(char*, char *);
+extern void execute_229(char*, char *);
+extern void execute_249(char*, char *);
+extern void execute_252(char*, char *);
+extern void execute_253(char*, char *);
+extern void execute_254(char*, char *);
+extern void execute_255(char*, char *);
+extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+extern void transaction_6(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_20(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_55(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_56(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_57(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_58(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_59(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_60(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[40] = {(funcp)execute_256, (funcp)execute_257, (funcp)execute_250, (funcp)execute_45, (funcp)execute_47, (funcp)execute_81, (funcp)execute_82, (funcp)execute_83, (funcp)execute_84, (funcp)execute_85, (funcp)execute_86, (funcp)execute_87, (funcp)execute_88, (funcp)execute_215, (funcp)execute_218, (funcp)execute_219, (funcp)execute_220, (funcp)execute_221, (funcp)execute_222, (funcp)execute_223, (funcp)execute_224, (funcp)execute_225, (funcp)execute_226, (funcp)execute_227, (funcp)execute_228, (funcp)execute_229, (funcp)execute_249, (funcp)execute_252, (funcp)execute_253, (funcp)execute_254, (funcp)execute_255, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_6, (funcp)transaction_20, (funcp)transaction_55, (funcp)transaction_56, (funcp)transaction_57, (funcp)transaction_58, (funcp)transaction_59, (funcp)transaction_60};
+const int NumRelocateId= 40;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/calc_behav/xsim.reloc",  (void **)funcTab, 40);
+	iki_vhdl_file_variable_register(dp + 45464);
+	iki_vhdl_file_variable_register(dp + 45520);
+
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/calc_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/calc_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/calc_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/calc_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/calc_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
